8abf72f95e3cd0d786ea4e022ab4506b488a1bf8
Rudzky <lubieprogramowanie@gmail.com>
2021-06-28 17:24:46 +0200

    Add enums example

---
+++ src/enums.ts
@@ -0,0 +1,33 @@
+enum LoadingState {
+  beforeLoad = "beforeLoad",
+  loading = "loading",
+  loaded = "loaded",
+}
+
+const englishLoadingStates = {
+  [LoadingState.beforeLoad]: "Before Load",
+};
+
+const isLoaded = (state: LoadingState) => state === LoadingState.loading;
+
+console.log(isLoaded(LoadingState.beforeLoad));
+
+// Literal Types
+
+function rollDice(dice: 1 | 2 | 3): number {
+  let pip = 0;
+  for (let i = 0; i < dice; i++) {
+    pip += Math.floor(Math.random() * 5 + 1);
+  }
+  return pip;
+}
+
+console.log(rollDice(3));
+
+function sendEvent(name: "addToCart", data: { productID: number }): void;
+function sendEvent(name: "checkout", data: { cartCount: number }): void;
+function sendEvent(name: string, data: unknown): void {
+  console.log(`${name} ${JSON.stringify(data)}`);
+}
+
+sendEvent("addToCart", { productID: 123213 });
